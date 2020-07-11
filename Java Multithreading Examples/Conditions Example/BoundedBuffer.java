a72b26df232d326a611dd1d9eec94ffa86525ec5
C734R <duypham12241999@gmail.com>
2020-07-10 17:04:04 -0700

    added comments

--- Java Multithreading Examples/Conditions Example/BoundedBuffer.java
+++ Java Multithreading Examples/Conditions Example/BoundedBuffer.java
@@ -26,39 +26,49 @@ public class BoundedBuffer {
     }
 
     public void deposit(String data) throws InterruptedException {
+        // lock to ensure that multiple threads aren't running deposit at the same time
         lock.lock();
 
         try {
+            // if the buffer is already full, we just wait until it's no longer full and continue
             while (count == capacity) {
                 notFull.await();
             }
 
+            // if the buffer is not full, add the data to the rear of the buffer
             buffer[rear] = data;
             rear = (rear + 1) % capacity;
             count++;
 
+            // since there's data in the buffer, we notify threads that were waiting to fetch that there's data in the buffer
             notEmpty.signal();
         } finally {
+            // unlock to allow other threads to deposit
             lock.unlock();
         }
     }
 
     public String fetch() throws InterruptedException {
+        // lock to ensure that multiple threads aren't running fetch at the same time
         lock.lock();
 
         try {
+            // if there's nothing in the buffer, wait until there's something in the buffer before continuing 
             while (count == 0) {
                 notEmpty.await();
             }
 
+            // if the buffer is  not empty, remove data from the front of the buffer
             String result = buffer[front];
             front = (front + 1) % capacity;
             count--;
 
+            // since we just removed data from the buffer, it's no longer full. notify other threads that were trying to deposit that they can now get data
             notFull.signal();
 
             return result;
         } finally {
+            // unlock to allow other threads to fetch
             lock.unlock();
         }
     }
